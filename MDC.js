c7d6fd65543d14b02ce2a34e3b872369206edd6a
Joseane-Guedes <joseane_23@hotmail.com>
2024-08-31 17:19:47 -0300

    First commit

---
+++ MDC.js
@@ -0,0 +1,11 @@
+//Calcule o MDC (máximo divisor comum) entre dois números.
+
+function mdc(a, b) {
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    return a;
+}
+
+// Exemplo de uso
+console.log(mdc(48, 18)); // Saída: 6
